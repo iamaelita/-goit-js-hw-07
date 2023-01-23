7501cbf25613fac4f73c2134ecd82a96d3538612
Aelita Deinychenko <amm-aelita@ukr.net>
2023-01-23 18:07:57 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+
+
+const galleryListRefs = document.querySelector('.gallery');
+const listMarkup = galleryItems
+  .map(img => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+galleryListRefs.innerHTML = listMarkup;
+galleryListRefs.addEventListener('click', openModal);
+
+function openModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: instance => {
+        window.addEventListener('keydown', closeModal);
+      },
+      onClose: instance => {
+        window.removeEventListener('keydown', closeModal);
+      },
+    }
+  );
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+  instance.show();
+}
