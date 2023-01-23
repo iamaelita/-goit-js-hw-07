7e64c9c6052ea69b9a4a4f40d11be5194364be3b
Aelita Deinychenko <amm-aelita@ukr.net>
2023-01-23 18:37:12 +0200

    task 02 is finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
+
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListRefs = document.querySelector('.gallery');
+const listMarkup = galleryItems
+  .map(img => {
+    return `<a class="gallery__item" href="${img.original}">
+  <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+</a>`;
+  })
+  .join('');
+galleryListRefs.innerHTML = listMarkup;
+
+galleryListRefs.addEventListener('click', openModal);
+
+function openModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
