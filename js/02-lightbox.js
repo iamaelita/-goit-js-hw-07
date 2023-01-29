f2a3ef10f898547c8f827af76e231520e7f31c8c
Aelita Deinychenko <amm-aelita@ukr.net>
2023-01-29 11:47:17 +0200

    updating task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,35 +1,35 @@
 
 
-import { galleryItems } from './gallery-items.js';
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
 
-const galleryListRefs = document.querySelector('.gallery');
-const listMarkup = galleryItems
-  .map(img => {
-    return `<a class="gallery__item" href="${img.original}">
-  <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
-</a>`;
-  })
-  .join('');
-galleryListRefs.insertAdjacentHTML("beforeend", listMarkup);
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
 
-galleryListRefs.addEventListener('click', openModal);
-
-function openModal(event) {
-  event.preventDefault();
-  if (!event.target.classList.contains('gallery__image')) {
-    return;
-  }
-
-  function closeModal(event) {
-    if (event.code === 'Escape') {
-      instance.close();
-    }
-  }
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
 }
-const lightbox = new SimpleLightbox('.gallery a', {
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
   captionDelay: 250,
-  captionsData: 'alt',
 });
+
+console.log(galleryItems);
